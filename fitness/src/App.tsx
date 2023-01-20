5f82e35dc5d63879f57916c3e3055f30727a9452
1305Burov <1305burov@gmail.com>
2023-01-20 23:53:08 +0200

    Vite + TS + TailWind

---
+++ fitness/src/App.tsx
@@ -0,0 +1,38 @@
+import Navbar from '@/scenes/Navbar';
+import Home from '@/scenes/Home';
+import Benefits from './scenes/Benefits/index';
+
+import { useEffect, useState } from 'react';
+import { SelectedPage } from '@/shared/types';
+import OurClasses from './scenes/OurClasses';
+
+function App() {
+    const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home); 
+    const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true); 
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY === 0) {
+                setIsTopOfPage(true);
+                setSelectedPage(SelectedPage.Home);
+            }else {
+                setIsTopOfPage(false);
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+    
+
+    return (
+        <div className="App bg-gray-20">
+            <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Home setSelectedPage={setSelectedPage} />
+            <Benefits setSelectedPage={setSelectedPage} />
+            <OurClasses setSelectedPage={setSelectedPage} />
+        </div>
+    )
+}
+
+export default App
